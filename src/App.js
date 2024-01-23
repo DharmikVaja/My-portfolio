4452d9d3d9d6664c4d9beb025c573708bdbe26d2
DharmikVaja <vajadharmikk@gmail.com>
2024-01-23 17:50:07 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // import logo from './logo.svg';
 // import "./App.css";
+import React from "react";
+import "./component/style.css";
+import Navbar from "./component/Navbar.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+import App from "../App";
+// import logo from "../assets/logo.png";
+
+function Navbar() {
+  return <></>;
+}
+
+export default Navbar;
